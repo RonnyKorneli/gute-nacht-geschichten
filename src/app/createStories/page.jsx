18c90ff89cca5914476d10113f6fa710331732b1
Ronny Korneli <ronnykorneli@googlemail.com>
2023-11-13 14:05:22 +0100

    Created an Uploaded a feature where I can upload images to S3 Bucket

--- src/app/createStories/page.jsx
+++ src/app/createStories/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useState } from 'react';
+import UploadImageToS3 from '../upload_image_s3/pages';
 
 function CreateStories(){
 
@@ -9,6 +10,24 @@ function CreateStories(){
     const [body, setBody] = useState('');
     const [readTime, setReadTime] = useState('');
     const [recomendedAge, setRecomendedAge] = useState('');
+    const [file, setFile] = useState(null);
+    const [imageUrl, setImageUrl] = useState('');
+
+    
+    console.log(imageUrl, "imageUrl");
+    console.log(author, "author");
+    console.log(file, "File");
+
+    const handleFileChange = async (e) => {
+        e.preventDefault()
+        const file = e.target.files[0];
+        setFile(file);
+
+        console.log(file, "chnges herererererer");
+        
+    }
+
+    
 
     const submitHandler = async (e) => {
         e.preventDefault();
@@ -17,17 +36,33 @@ function CreateStories(){
             author: author, 
             readTime: readTime,
             recomendedAge: recomendedAge,
-            body: body
+            body: body,
+            imageUrl: imageUrl
         };
-       console.log(story);
+
+        console.log(story, "storyObject");
 
         try {
+            //getting signed url from server
+            const {url} = await fetch('http://localhost:2000/api/stories/s3Url').then(res => res.json());
+            console.log(url);
+            //Upload image to S3
+            await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': "multi-part/form-data"
+                },
+                body: file
+            });
+            const imageUrl = url.split('?')[0];
+            const newStory = {...story, imageUrl: imageUrl};
+           
             const res = await fetch('http://localhost:2000/api/stories/create', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(story)
+                body: JSON.stringify(newStory)
             });
             const json = await res.json();
             console.log(json);
@@ -35,6 +70,7 @@ function CreateStories(){
             console.error("Error creating story!", error);
             throw error;
         }
+       
 
        /*  setAuthor('');
         setTitle('');
@@ -42,23 +78,27 @@ function CreateStories(){
         setRecomendedAge(null); */
     }
 
+   
 
     return (
         <div className='w-full bg-slate-400 h-full flex justify-start items-center flex-col'>
             <h1 className='text-xl mb-12 mt-12'>Create Stories</h1>
             <form 
                 action=""
+                name="createStory"
                 className='w-1/2 bg-white p-5 rounded-lg flex flex-col justify-center items-center'
             >
                 
                 <input 
                     type="text" 
+                    name='title'
                     className='border mb-3 w-[90%] border-gray-400 p-2 rounded-lg'
                     placeholder='Title'
                     onChange={(e) => setTitle(e.target.value)}
                 />
                 <input 
                     type="text" 
+                    name='author'
                     className='border mb-3 w-[90%] border-gray-400 p-2 rounded-lg'
                     placeholder='Author'
                     onChange={(e) => setAuthor(e.target.value)}
@@ -66,6 +106,7 @@ function CreateStories(){
                 />
                 <input 
                     type="number" 
+                    name='readTime'
                     className='border mb-3 w-[90%] border-gray-400 p-2 rounded-lg'
                     placeholder='Read Time'
                     onChange={(e) => setReadTime(e.target.value)}
@@ -73,13 +114,14 @@ function CreateStories(){
                 />
                 <input 
                     type="number" 
+                    name='recomendedAge'
                     className='border mb-3 w-[90%] border-gray-400 p-2 rounded-lg'
                     placeholder='Recomended Age'
                     onChange={(e) => setRecomendedAge(e.target.value)}
 
                 />
                 <textarea 
-                    name="" 
+                    name="story" 
                     id="" 
                     cols="30" 
                     rows="11"
@@ -88,13 +130,26 @@ function CreateStories(){
                     onChange={(e) => setBody(e.target.value)}
                 >
                 </textarea>
+
+                <input 
+                    type="file" 
+                    accept="image/*"
+                    onChange={handleFileChange} 
+                    name="image-file"
+                />
+
                 <button
                     className=' w-[90%] bg-blue-500 hover:bg-blue-800 text-white p-2 rounded-lg'
                     onClick={submitHandler}
                 >
                     Submit Story
                 </button>
+
             </form>
+
+           
+
+          
         </div>
     )
 }
